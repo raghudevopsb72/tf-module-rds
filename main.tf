936bed5924ef8127f3c6578a20709c0dd6398479
Raghu K <raghuk.vit@gmail.com>
2023-05-29 06:18:35 +0530

    Update tf-module-rds/

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "main" {
+  name        = "${var.name}-${var.env}-sg"
+  description = "${var.name}-${var.env}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "DOCDB"
+    from_port   = var.port_no
+    to_port     = var.port_no
+    protocol    = "tcp"
+    cidr_blocks = var.allow_db_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(var.tags, { Name = "${var.name}-${var.env}-sg" })
+}
